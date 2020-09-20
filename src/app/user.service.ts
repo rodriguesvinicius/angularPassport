0508e21fc70e931b3490244771cd4a947b97dbda
Vinicius Alves Rodrigues da Silva <vinicius.silva@yandeh.com.br>
2020-09-20 02:15:30 -0300

    Update System

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,39 +1,40 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class UserService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
+  private endpoint = 'https://backendangularteste.herokuapp.com'
 
-  register(body:any){
-    return this._http.post('http://127.0.0.1:3000/users/register',body,{
-      observe:'body',
-      headers:new HttpHeaders().append('Content-Type','application/json')
+  register(body: any) {
+    return this._http.post(this.endpoint + '/users/register', body, {
+      observe: 'body',
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
     });
   }
 
-  login(body:any){
-    return this._http.post('http://127.0.0.1:3000/users/login',body,{
-      observe:'body',
-      withCredentials:true,
-      headers:new HttpHeaders().append('Content-Type','application/json')
+  login(body: any) {
+    return this._http.post(this.endpoint + '/users/login', body, {
+      observe: 'body',
+      withCredentials: true,
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
     });
   }
 
-  user(){
-    return this._http.get('http://127.0.0.1:3000/users/user',{
-      observe:'body',
-      withCredentials:true,
-      headers:new HttpHeaders().append('Content-Type','application/json')
+  user() {
+    return this._http.get(this.endpoint + '/users/user', {
+      observe: 'body',
+      withCredentials: true,
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
     })
   }
 
-  logout(){
-    return this._http.get('http://127.0.0.1:3000/users/logout',{
-      observe:'body',
-      withCredentials:true,
-      headers:new HttpHeaders().append('Content-Type','application/json')
+  logout() {
+    return this._http.get(this.endpoint + '/users/logout', {
+      observe: 'body',
+      withCredentials: true,
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
     })
   }
 
